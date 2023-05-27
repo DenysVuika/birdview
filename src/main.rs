559c70198b0907155bfe24a25aa4a029da3140b3
Denys Vuika <denys.vuika@gmail.com>
2023-05-27 18:02:58 +0100

    use iterators

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use std::{env, process};
 use birdview::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -13,7 +12,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = birdview::run(config) {
-        println!("Application error {e}");
+        eprintln!("Application error {e}");
         process::exit(1);
     }
 }
