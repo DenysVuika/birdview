6a19f03e584d050242e76d6dae2f8166701ba8ce
Denys Vuika <denys.vuika@gmail.com>
2023-05-27 14:04:05 +0100

    improved example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use std::env;
+use std::{env, process};
+use std::error::Error;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let input_file = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", input_file);
+    if let Err(e) = run(config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(input_file)
-        .expect("Should have been able to read file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
