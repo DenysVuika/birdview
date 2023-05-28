15bef4d3ff94f17d8a414ab2baa5fd8728910a39
Denys Vuika <denys.vuika@gmail.com>
2023-05-28 09:24:38 +0100

    imporoved output formatting

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use std::{env, process};
 use walkdir::WalkDir;
 
-use birdview::Config;
+use birdview::{Config, extract_test_names};
 use birdview::fs;
 
 fn main() {
@@ -19,14 +19,6 @@ fn main() {
         eprintln!("Application error {e}");
         process::exit(1);
     }
-
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.file_path);
-
-    // if let Err(e) = birdview::run(config) {
-    //     eprintln!("Application error {e}");
-    //     process::exit(1);
-    // }
 }
 
 fn inspect_dir(working_dir: &Path) -> Result<(), Box<dyn Error>> {
@@ -34,6 +26,8 @@ fn inspect_dir(working_dir: &Path) -> Result<(), Box<dyn Error>> {
     let mut spec_files_count = 0;
     let mut test_files_count = 0;
     let mut package_files_count = 0;
+    let mut total_unit_tests_count = 0;
+    let mut total_e2e_tests_count = 0;
 
     for entry in walker
         .filter_entry(|e| fs::is_not_hidden(e) && !fs::is_excluded(e))
@@ -43,18 +37,33 @@ fn inspect_dir(working_dir: &Path) -> Result<(), Box<dyn Error>> {
 
         if f_name.ends_with(".spec.ts") {
             spec_files_count += 1;
-            // println!("{}", entry.path().display());
+            println!("{}", entry.path().strip_prefix(working_dir)?.display());
+
+            let contents = std::fs::read_to_string(entry.path())?;
+            for test_name in extract_test_names(&contents) {
+                total_unit_tests_count += 1;
+                println!("  ├── {test_name}");
+            }
+
         }
         if f_name.ends_with(".test.ts") {
             test_files_count += 1;
+            println!("{}", entry.path().strip_prefix(working_dir)?.display());
+
+            let contents = std::fs::read_to_string(entry.path())?;
+            for test_name in extract_test_names(&contents) {
+                total_e2e_tests_count += 1;
+                println!("  ├── {test_name}");
+            }
         }
+
         if f_name == "package.json" {
             package_files_count += 1;
         }
     }
 
-    println!("Found .spec.ts files: {}", spec_files_count);
-    println!("Found .test.ts files: {}", test_files_count);
+    println!("Found .spec.ts files: {} ({} cases)", spec_files_count, total_unit_tests_count);
+    println!("Found .test.ts files: {}, ({} cases)", test_files_count, total_e2e_tests_count);
     println!("Found package.json files: {}", package_files_count);
 
     Ok(())
