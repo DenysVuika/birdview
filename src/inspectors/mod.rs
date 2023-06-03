decb47bc8f6b43b33cba9f6c84f5790e60ff9737
Denys Vuika <denys.vuika@gmail.com>
2023-06-03 12:20:14 +0100

    simplify file names

--- src/inspectors/mod.rs
+++ src/inspectors/mod.rs
@@ -1,14 +1,14 @@
-mod angular_inspector;
-mod e2e_test_inspector;
-mod file_inspector;
-mod markdown_inspector;
-mod package_json_inspector;
-mod unit_test_inspector;
+mod angular;
+mod e2e_test;
+mod inspector;
+mod markdown;
+mod package_json;
+mod unit_test;
 mod utils;
 
-pub use angular_inspector::AngularInspector;
-pub use e2e_test_inspector::EndToEndTestInspector;
-pub use file_inspector::FileInspector;
-pub use markdown_inspector::MarkdownInspector;
-pub use package_json_inspector::PackageJsonInspector;
-pub use unit_test_inspector::UnitTestInspector;
+pub use angular::AngularInspector;
+pub use e2e_test::EndToEndTestInspector;
+pub use inspector::FileInspector;
+pub use markdown::MarkdownInspector;
+pub use package_json::PackageJsonInspector;
+pub use unit_test::UnitTestInspector;
