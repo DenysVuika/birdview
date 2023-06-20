6c49c83b2518294176dd585598bd504ac66948fc
Denys Vuika <denys.vuika@gmail.com>
2023-06-20 19:04:30 +0100

    display report based on the snapshot

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,5 @@
-use crate::db;
-use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
-use anyhow::Result;
+use crate::{db, report};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder, Result};
 use futures::{join, TryFutureExt};
 use rusqlite::Connection;
 use std::path::PathBuf;
@@ -19,8 +18,9 @@ pub async fn run_server(working_dir: PathBuf, open: bool) -> Result<()> {
             }))
             .wrap(middleware::Compress::default())
             .wrap(middleware::Logger::default())
+            .service(web::scope("/api").service(list_projects))
             .service(index)
-            .service(get_projects)
+            .service(report_details)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -32,21 +32,33 @@ pub async fn run_server(working_dir: PathBuf, open: bool) -> Result<()> {
     Ok(())
 }
 
-#[get("/api/projects")]
-async fn get_projects(data: web::Data<AppState>) -> actix_web::Result<impl Responder> {
-    // let template = include_str!("assets/html/index.html");
+#[get("/projects")]
+async fn list_projects(data: web::Data<AppState>) -> Result<impl Responder> {
     let conn = &data.connection;
     let projects = db::get_projects(conn).unwrap();
     Ok(web::Json(projects))
-
-    // let data = format!("window.data = {};", json_string);
-    // let template = template.replace("// <birdview:DATA>", &data);
-    // HttpResponse::Ok().body(template)
 }
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/projects/{project}/{snapshot}")]
+async fn report_details(
+    path: web::Path<(String, i64)>,
+    data: web::Data<AppState>,
+) -> Result<HttpResponse> {
+    let params = path.into_inner();
+    let conn = &data.connection;
+    let template = include_str!("assets/html/index.html");
+    let data = report::generate_report(conn, params.1).unwrap();
+    let json_string = serde_json::to_string_pretty(&data)?;
+
+    let result_data = format!("window.data = {};", json_string);
+    let result_template = template.replace("// <birdview:DATA>", &result_data);
+
+    Ok(HttpResponse::Ok().body(result_template))
 }
 
 async fn open_report(open: bool) -> std::io::Result<()> {
